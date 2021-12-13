c1e669f6df63771b83d415b7ba511295c6585517
TadeasCehlarik <96028699+TadeasCehlarik@users.noreply.github.com>
2021-12-13 15:37:09 +0100

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Web3 from 'web3';
+import React from 'react';
+import Crud from 'contracts/Crud.json';
+import CrudBuild from 'contracts/Crud.json';
+
+
+function App() {
+
+
+  }
+
+export default App;
